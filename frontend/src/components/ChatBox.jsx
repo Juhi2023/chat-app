944403b0e2fc20fc9207b704a351f71facdff92b
Juhi Sahu <juhisahu2023@gmail.com>
2022-10-17 02:00:06 +0530

    add: deployment lines

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import { useState } from 'react';
 import typingGif from '../assets/typing.gif'
 
-const ENDPOINT ="http://localhost:5000";
+const ENDPOINT ="https://talks-up.herokuapp.com/";
 var socket, selectedChatCompare;
 
 function ChatBox(props) {
